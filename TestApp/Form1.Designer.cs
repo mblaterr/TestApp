0275eafd937f3a8ed4b7003e2d1665c7e600e290
mblaterr <pirogovb185@gmail.com>
2026-01-20 16:03:15 +0300

    Добавлены обработчики кнопок

--- TestApp/Form1.Designer.cs
+++ TestApp/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Отобразить";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -57,6 +58,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Выход";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
